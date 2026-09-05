68ae4846cfb8d5d42fe0fffb35fff984d66723fe
HyperCriSiS <dev.github@w3b.world>
2026-09-05 18:22:49 +0200

    api: migrate adlist repository to generated service

--- lib/data/repositories/api/v6/adlist_repository.dart
+++ lib/data/repositories/api/v6/adlist_repository.dart
@@ -1,22 +1,25 @@
 import 'package:pi_hole_client/data/mapper/v6/adlist_mapper.dart';
 import 'package:pi_hole_client/data/mapper/v6/list_search_mapper.dart';
+import 'package:pi_hole_client/data/model/v6/lists/lists.dart' as legacy_lists;
+import 'package:pi_hole_client/data/model/v6/lists/search.dart' as legacy_search;
 import 'package:pi_hole_client/data/repositories/api/interfaces/adlist_repository.dart';
 import 'package:pi_hole_client/data/repositories/api/v6/base_v6_sid_repository.dart';
 import 'package:pi_hole_client/data/repositories/utils/call_with_retry.dart';
-import 'package:pi_hole_client/data/services/api/pihole_v6_api_client.dart';
+import 'package:pi_hole_client/data/services/api/wrappers/pihole_v6_service.dart';
 import 'package:pi_hole_client/domain/model/enums.dart';
 import 'package:pi_hole_client/domain/model/list/adlist.dart';
 import 'package:pi_hole_client/domain/model/list/list_search_result.dart';
+import 'package:pihole_v6_api/pihole_v6_api.dart';
 import 'package:result_dart/result_dart.dart';
 
 class AdlistRepositoryV6 extends BaseV6SidRepository
     implements AdListRepository {
   AdlistRepositoryV6({
-    required PiholeV6ApiClient client,
+    required PiholeV6Service service,
     required super.sessionCache,
-  }) : _client = client;
+  }) : _service = service;
 
-  final PiholeV6ApiClient _client;
+  final PiholeV6Service _service;
 
   @override
   Future<Result<List<Adlist>>> fetchAdlists({
@@ -26,8 +29,13 @@ class AdlistRepositoryV6 extends BaseV6SidRepository
     return runWithResultRetry<List<Adlist>>(
       action: () async {
         final sid = await getSid();
-        final result = await _client.getLists(sid, adlist: adlist, type: type);
-        return result.map((e) => e.toDomain());
+        _service.setSid(sid);
+        final result = adlist == null
+            ? await _service.getAllLists(type: type?.name)
+            : await _service.getLists(list: adlist, type: type?.name);
+        return result.map(
+          (e) => legacy_lists.Lists.fromJson(e.toJson()).toDomain(),
+        );
       },
       onRetry: (_, e) => renewSidIfExpired(e),
     );
@@ -44,15 +52,19 @@ class AdlistRepositoryV6 extends BaseV6SidRepository
     return runWithResultRetry<Adlist>(
       action: () async {
         final sid = await getSid();
-        final result = await _client.postLists(
-          sid,
-          address: address,
-          type: type,
-          groups: groups,
-          comment: comment,
-          enabled: enabled,
+        _service.setSid(sid);
+        final result = await _service.addList(
+          type: type.name,
+          body: ListsPost(
+            address: StringOrList.fromString(address),
+            groups: groups,
+            comment: comment,
+            enabled: enabled,
+          ),
         );
-        return result.map((e) => e.toSingleDomain());
+        return result.map(
+          (e) => legacy_lists.Lists.fromJson(e.toJson()).toSingleDomain(),
+        );
       },
       onRetry: (_, e) => renewSidIfExpired(e),
     );
@@ -69,15 +81,20 @@ class AdlistRepositoryV6 extends BaseV6SidRepository
     return runWithResultRetry<Adlist>(
       action: () async {
         final sid = await getSid();
-        final result = await _client.putLists(
-          sid,
-          adlist: address,
-          type: type,
-          groups: groups,
-          comment: comment,
-          enabled: enabled,
+        _service.setSid(sid);
+        final result = await _service.replaceList(
+          list: address,
+          type: type.name,
+          body: ListsPut(
+            type: _generatedType(type),
+            groups: groups,
+            comment: comment,
+            enabled: enabled,
+          ),
         );
-        return result.map((e) => e.toSingleDomain());
+        return result.map(
+          (e) => legacy_lists.Lists.fromJson(e.toJson()).toSingleDomain(),
+        );
       },
       onRetry: (_, e) => renewSidIfExpired(e),
     );
@@ -88,12 +105,8 @@ class AdlistRepositoryV6 extends BaseV6SidRepository
     return runWithResultRetry<Unit>(
       action: () async {
         final sid = await getSid();
-        final result = await _client.deleteLists(
-          sid,
-          adlist: address,
-          type: type,
-        );
-        return result.map((_) => unit);
+        _service.setSid(sid);
+        return _service.deleteList(list: address, type: type.name);
       },
       onRetry: (_, e) => renewSidIfExpired(e),
     );
@@ -108,15 +121,24 @@ class AdlistRepositoryV6 extends BaseV6SidRepository
     return runWithResultRetry<ListSearchResult>(
       action: () async {
         final sid = await getSid();
-        final result = await _client.getSearch(
-          sid,
+        _service.setSid(sid);
+        final result = await _service.searchDomainInLists(
           domain: domain,
+          n: limit,
           partial: partial,
-          limit: limit,
         );
-        return result.map((e) => e.toDomain());
+        return result.map(
+          (e) => legacy_search.Search.fromJson(e.toJson()).toDomain(),
+        );
       },
       onRetry: (_, e) => renewSidIfExpired(e),
     );
+  }
+
+  ListsPutTypeEnum _generatedType(ListType type) {
+    return switch (type) {
+      ListType.allow => ListsPutTypeEnum.allow,
+      ListType.block => ListsPutTypeEnum.block,
+    };
   }
 }
